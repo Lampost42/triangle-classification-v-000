1d7069d86cbfd379ed1e82e7b02bc3cd56a9e600
Trevor Russell <truss.42@gmail.com>
2018-06-21 20:29:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,10 @@ class Triangle
   
   def kind
     if @sides.any?{|side| side <= 0} || (@side1 + @side2) < @side3 || (@side1 + @side3) < @side2 || (@side3 + @side2) < @side1
-      raise
-      
+      begin
+        raise TriangleError
+          rescue TriangleError => error
+            error.puts
     if @side1 == @side2 && @side1 == @side3
       :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
