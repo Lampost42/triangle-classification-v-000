23aed6c72d28c11fc2260a9dee51d7e77aa69368
Trevor Russell <truss.42@gmail.com>
2018-06-21 20:26:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,19 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
+  
+  @sides = []
+  
   def initialize(side1, side2, side3)
     @side3 = side3
     @side2 = side2
     @side1 = side1
+    @sides << side1
+    @sides << side2
+    @sides << side3
   end
   
   def kind
+    if @sides.each{|side| side <= 0} 
     if @side1 == @side2 && @side1 == @side3
       :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
@@ -14,6 +21,9 @@ class Triangle
     else
       :scalene
     end
+  end
+  
+  class TriangleError < StandardError
   end
   
 end
