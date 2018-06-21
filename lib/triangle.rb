f21bb5555628fee762a8608adfa31da35a500f52
Trevor Russell <truss.42@gmail.com>
2018-06-21 20:31:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,9 +16,10 @@ class Triangle
     if @sides.any?{|side| side <= 0} || (@side1 + @side2) < @side3 || (@side1 + @side3) < @side2 || (@side3 + @side2) < @side1
       begin
         raise TriangleError
-          rescue TriangleError => error
-            puts error.message
-    if @side1 == @side2 && @side1 == @side3
+      rescue TriangleError => error
+          puts error.message
+      end
+    elsif @side1 == @side2 && @side1 == @side3
       :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       :isosceles
